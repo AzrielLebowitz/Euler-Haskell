a3e442d92548345563e63e7bb9c0f48f66db2584
Azriel Lebowitz <azriellebowitz@gmail.com>
2023-07-16 11:36:13 +0300

    feat(Euler10): euler10 done

--- src/Euler/Euler10.hs
+++ src/Euler/Euler10.hs
@@ -2,7 +2,8 @@ module Euler.Euler10 (
         euler10
 ) where
 import Euler.EulerType (EulerType)
+import MathUtils (primes)
 
 
 euler10 :: EulerType
-euler10 = -1
+euler10 = sum $ takeWhile(<2000000) primes
