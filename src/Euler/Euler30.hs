f943529b9499eed7f9b5797d8cccd3adf6668fda
Azriel Lebowitz <azriellebowitz@gmail.com>
2023-07-18 14:40:21 +0300

    feat(Euler30): euler30 done

--- src/Euler/Euler30.hs
+++ src/Euler/Euler30.hs
@@ -2,7 +2,13 @@ module Euler.Euler30 (
         euler30
 ) where
 import Euler.EulerType (EulerType)
+import Data.Char (digitToInt)
 
+limit :: Int
+limit = snd $ head $ dropWhile (\(a,b) -> a > b) $ zip (map (9^5*) [1..]) (map (10^) [1..])
+
+fifth :: Int -> Int
+fifth = sum . map ((^5) . digitToInt) . show
 
 euler30 :: EulerType
-euler30 = -1
+euler30 = sum $ filter (\n -> n == fifth n) [2..limit]
