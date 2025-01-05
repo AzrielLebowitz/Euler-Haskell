15d080a3a4c90941aeaf9f1a4c31d1f9cbea163d
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-05 21:24:03 +0200

    fix(Euler1): I can't actually be *that* stupid, right?

--- src/Euler/Euler1.hs
+++ src/Euler/Euler1.hs
@@ -1,12 +1,13 @@
-module Euler.Euler1 ( 
-       euler1
-        ) where
-  
-import Data.List()
+module Euler.Euler1
+  ( euler1,
+  )
+where
+
+import Data.List ()
 import Euler.EulerType (EulerType)
 
 sumOfArrayOfMulti :: Int -> Int
-sumOfArrayOfMulti untilMax = sum [x | x <- [1..untilMax], x `mod` 3 == 0 || x `mod` 5 == 0]
+sumOfArrayOfMulti untilMax = sum [x | x <- [1 .. untilMax], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 euler1 :: EulerType
-euler1 = sumOfArrayOfMulti 1000
+euler1 = sumOfArrayOfMulti 999
