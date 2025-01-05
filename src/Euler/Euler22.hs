f81d561f41d8984a889ba3d3b74a587ac09e0083
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-05 21:21:38 +0200

    fix: added .txt files for 22 & 42 & changed path

--- src/Euler/Euler22.hs
+++ src/Euler/Euler22.hs
@@ -1,17 +1,18 @@
-module Euler.Euler22 (
-        euler22
-) where
-import Euler.EulerType (EulerType)
+module Euler.Euler22
+  ( euler22,
+  )
+where
+
 import Data.Char (ord)
 import Data.List (sort)
-import Utils (getFile, convertIO)
-
+import Euler.EulerType (EulerType)
+import Utils (convertIO, getFile)
 
 names :: [String]
-names = sort $ read $ "[" ++ convertIO(getFile "src\\Euler\\0022_names.txt") ++ "]"
+names = sort $ read $ "[" ++ convertIO (getFile "src/Euler/0022_names.txt") ++ "]"
 
 score :: String -> Int -> Int
 score w i = (i *) . sum . map (\c -> ord c - ord 'A' + 1) $ w
 
 euler22 :: EulerType
-euler22 = sum $ zipWith score names [1..]
+euler22 = sum $ zipWith score names [1 ..]
--- src/Euler/Euler42.hs
+++ src/Euler/Euler42.hs
@@ -1,14 +1,14 @@
-module Euler.Euler42 (
-        euler42
-) where
-import Euler.EulerType (EulerType)
-import Utils (getFile, convertIO)
-import MathUtils (isTriagulerNumber, wordValue)
+module Euler.Euler42
+  ( euler42,
+  )
+where
 
+import Euler.EulerType (EulerType)
+import MathUtils (isTriagulerNumber, wordValue)
+import Utils (convertIO, getFile)
 
 wordList :: [String]
-wordList = read $ "[" ++ convertIO(getFile "src\\files\\0042_words.txt") ++ "]"
-
+wordList = read $ "[" ++ convertIO (getFile "src/Euler/0042_words.txt") ++ "]"
 
 euler42 :: EulerType
 euler42 = length $ filter isTriagulerNumber $ map wordValue wordList
