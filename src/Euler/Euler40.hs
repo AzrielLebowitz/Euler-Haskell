0644380db35f81bb92e4a357d8586fb8e9ecb244
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-08 17:28:35 +0200

    feat(Euler40): problem 40 solved for now, has warnnings

--- src/Euler/Euler40.hs
+++ src/Euler/Euler40.hs
@@ -1,8 +1,10 @@
-module Euler.Euler40 (
-        euler40
-) where
+module Euler.Euler40
+  ( euler40,
+  )
+where
+
+import Data.Char (digitToInt)
 import Euler.EulerType (EulerType)
 
-
 euler40 :: EulerType
-euler40 = -1
+euler40 = product . map digitToInt $ [concatMap show [1 ..] !! (10 ^ n - 1) | n <- [0 .. 6]]
