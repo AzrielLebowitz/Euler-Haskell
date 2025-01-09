844ef3504e7bb3d0e90aeb2ddb22fabe4defdb13
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-09 09:24:01 +0200

    feat(Euler40): problem 40 solved. a bit slow but I reversed the num array for the test and it takes 4.1-4.2 seconds

--- src/Euler/Euler43.hs
+++ src/Euler/Euler43.hs
@@ -1,8 +1,20 @@
-module Euler.Euler43 (
-        euler43
-) where
-import Euler.EulerType (EulerType)
+module Euler.Euler43
+  ( euler43,
+  )
+where
 
+import Data.List (permutations)
+import Euler.EulerType (EulerType)
+import MathUtils (primes)
+
+prime :: Int -> Int
+prime num = primes !! (num - 1)
+
+isDivisible :: Int -> String -> Bool
+isDivisible num s = read (take 3 $ drop num s) `mod` prime num == 0
+
+isFullPandigital :: String -> Bool
+isFullPandigital numStr = all (`isDivisible` numStr) [7, 6 .. 1] -- in reverse because of my need for speeeeeeed
 
 euler43 :: EulerType
-euler43 = -1
+euler43 = sum . map read . filter isFullPandigital $ permutations "1234567890"
