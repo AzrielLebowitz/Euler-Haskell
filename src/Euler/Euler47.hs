f1bd7281e006ea41305acdcd2579424e334a4c02
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-09 22:45:41 +0200

    fix(Euler47): whatever...

--- src/Euler/Euler47.hs
+++ src/Euler/Euler47.hs
@@ -8,7 +8,7 @@ import Euler.EulerType (EulerType)
 import MathUtils (primeFactors)
 
 isAns :: [Int] -> Bool
-isAns arr = arr !! 3 - head arr == 4
+isAns arr = arr !! 3 - head arr == 3
 
 euler47 :: EulerType
-euler47 = head . head . filter isAns . map (take 4) . tails . map fst . filter (\x -> snd x == 4) $ zip [1 ..] $ map (length . primeFactors) [210 ..]
+euler47 = head . head . filter isAns . map (take 4) . tails . map fst . filter (\x -> snd x == 4) $ zip [1 ..] $ map (length . primeFactors) [1 ..]
