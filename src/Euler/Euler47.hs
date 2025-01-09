970e1e5976af1a8f7e2ee35a941a384544468ab7
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-09 22:19:13 +0200

    feat(Euler47): problem 47 solved

--- src/Euler/Euler47.hs
+++ src/Euler/Euler47.hs
@@ -1,8 +1,14 @@
-module Euler.Euler47 (
-        euler47
-) where
-import Euler.EulerType (EulerType)
+module Euler.Euler47
+  ( euler47,
+  )
+where
 
+import Data.List (tails)
+import Euler.EulerType (EulerType)
+import MathUtils (primeFactors)
+
+isAns :: [Int] -> Bool
+isAns arr = arr !! 3 - head arr == 4
 
 euler47 :: EulerType
-euler47 = -1
+euler47 = head . head . filter isAns . map (take 4) . tails . map fst . filter (\x -> snd x == 4) $ zip [1 ..] $ map (length . primeFactors) [210 ..]
