c9934b4305ded55b4b7194159936a1a1889821fd
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-10 15:33:43 +0200

    feat(Euler19): problem 19 solved

--- src/Euler/Euler19.hs
+++ src/Euler/Euler19.hs
@@ -1,8 +1,15 @@
-module Euler.Euler19 (
-        euler19
-) where
+module Euler.Euler19
+  ( euler19,
+  )
+where
+
 import Euler.EulerType (EulerType)
 
+leapYear :: [Int]
+leapYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+nonLeapYear :: [Int]
+nonLeapYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 euler19 :: EulerType
-euler19 = -1
+euler19 = length . filter (== 1) . map (`mod` 7) . scanl (+) 2 . concat $ replicate 3 nonLeapYear ++ replicate 24 (concat $ leapYear : replicate 3 nonLeapYear)
