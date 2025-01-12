ed9a428d92746c6d2a879c762df2de74d6e00ec2
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-12 18:07:25 +0200

    feat(Euler56): problem 56 solved

--- src/Euler/Euler56.hs
+++ src/Euler/Euler56.hs
@@ -1,8 +1,10 @@
-module Euler.Euler56 (
-        euler56
-) where
-import Euler.EulerType (EulerType)
+module Euler.Euler56
+  ( euler56,
+  )
+where
 
+import Euler.EulerType (EulerType)
+import MathUtils (sumDigits)
 
 euler56 :: EulerType
-euler56 = -1
+euler56 = fromInteger $ maximum [(sumDigits . toInteger) (a ^ b) | a <- [1 .. 100], b <- [1 .. 100]]
