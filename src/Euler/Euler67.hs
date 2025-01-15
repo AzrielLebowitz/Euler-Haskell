449f030bd030fa4ae59ecf7e04e0b3a9e71c8a7a
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-15 18:46:00 +0200

    feat(Euler67): problem 67 solved, same code from 18

--- src/Euler/Euler67.hs
+++ src/Euler/Euler67.hs
@@ -1,8 +1,19 @@
-module Euler.Euler67 (
-        euler67
-) where
-import Euler.EulerType (EulerType)
+module Euler.Euler67
+  ( euler67,
+  )
+where
 
+import Euler.EulerType (EulerType)
+import Utils (convertIO, getFile)
+
+tri :: [[Int]]
+tri = map (map read . words) . lines $ convertIO $ getFile "src/Euler/0067_triangle.txt"
+
+f :: (Num a, Ord a) => a -> a -> a -> a
+f x y z = x + max y z
+
+g :: (Num d, Ord d) => [d] -> [d] -> [d]
+g xs ys = zipWith3 f xs ys $ tail ys
 
 euler67 :: EulerType
-euler67 = -1
+euler67 = head $ foldr1 g tri
