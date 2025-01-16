7f005e878b486993b7897340ba30e39d064fbf9a
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-16 17:36:42 +0200

    feat(Euler69): problem 69 solved. cant prove thats when totient is max, but hi, If the internet says so then...

--- src/Euler/Euler69.hs
+++ src/Euler/Euler69.hs
@@ -1,8 +1,11 @@
-module Euler.Euler69 (
-        euler69
-) where
-import Euler.EulerType (EulerType)
+module Euler.Euler69
+  ( euler69,
+  )
+where
 
+import Data.List (inits)
+import Euler.EulerType (EulerType)
+import MathUtils (primes)
 
 euler69 :: EulerType
-euler69 = -1
+euler69 = last . takeWhile (< 1000000) . map product . tail $ inits $ take 20 primes
