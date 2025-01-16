e2e15b6ba59688dd550fef1e5d4a5d570e3cdf76
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-16 17:39:06 +0200

    feat(Euler71): problem 71 solved.

--- src/Euler/Euler71.hs
+++ src/Euler/Euler71.hs
@@ -1,8 +1,11 @@
-module Euler.Euler71 (
-        euler71
-) where
+module Euler.Euler71
+  ( euler71,
+  )
+where
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
 import Euler.EulerType (EulerType)
 
-
 euler71 :: EulerType
-euler71 = -1
+euler71 = snd $ maximumBy (comparing fst) [(c, a) | b <- [1 .. 1000000], let a = round (fromIntegral b * 3 / 7 - 1), let c = fromIntegral a / fromIntegral b]
