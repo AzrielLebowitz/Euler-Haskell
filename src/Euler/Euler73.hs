e3d5ea0cee01b58c05e7a7af933b6db1129eb500
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-16 18:42:59 +0200

    feat(Euler73): problem 73 solved. to EZ

--- src/Euler/Euler73.hs
+++ src/Euler/Euler73.hs
@@ -1,8 +1,9 @@
-module Euler.Euler73 (
-        euler73
-) where
+module Euler.Euler73
+  ( euler73,
+  )
+where
+
 import Euler.EulerType (EulerType)
 
-
 euler73 :: EulerType
-euler73 = -1
+euler73 = length [() | b <- [1 .. 12000], a <- [(b `div` 3 + 1) .. (b `div` 2)], gcd a b == 1] - 1 -- -1 because of 1/2
