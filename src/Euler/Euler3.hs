687069362aadca23a0f542f356f600e874acfb53
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-20 13:54:56 +0200

    fix(Euler3): faster

--- src/Euler/Euler3.hs
+++ src/Euler/Euler3.hs
@@ -1,10 +1,11 @@
 module Euler.Euler3 (euler3) where
 
-import MathUtils(allPrimeFactors)
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+import MathUtils (primeFactors)
 
 num :: Int
 num = 600851475143
 
-
 euler3 :: Int
-euler3 = maximum $ allPrimeFactors num
+euler3 = fst . maximumBy (comparing fst) $ primeFactors num
