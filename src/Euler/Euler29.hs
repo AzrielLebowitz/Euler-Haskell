e62ee11928baac0794627c164b3f46b857ae53e3
AzrielLebowitz <azriellebowitz@gmail.com>
2025-01-20 12:45:21 +0200

    fix(Euler29): from 0.6s to 0s - I always test

--- src/Euler/Euler29.hs
+++ src/Euler/Euler29.hs
@@ -3,8 +3,8 @@ module Euler.Euler29
   )
 where
 
-import Data.IntSet (fromList, size)
+import Data.Set (fromList)
 import Euler.EulerType (EulerType)
 
 euler29 :: EulerType
-euler29 = size $ fromList [x ^ y | x <- [2 .. 100], y <- [2 .. 100]]
+euler29 = length $ fromList [x ^ y | x <- [2 .. 100], y <- [2 .. 100]]
